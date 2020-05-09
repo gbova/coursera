728e85db35dff61cffbf4244b597e76191091031
Gabriella Bova <bova3.0@gmail.com>
2020-05-08 20:47:35 -0400

    Add leaders & promotions endpoints

--- node_express/index.js
+++ node_express/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dish_router');
+const promoRouter = require('./routes/promotion_router');
+const leaderRouter = require('./routes/leader_router');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -14,6 +16,12 @@ app.use(bodyParser.json());
 
 // dishes endpoint
 app.use('/dishes', dishRouter)
+
+// promotions endpoint
+app.use('/promotions', promoRouter)
+
+// leaders endpoint
+app.use('/leaders', leaderRouter)
 
 app.use((req, res, next) => {
     res.statusCode = 200;
--- node_express/routes/dish_router.js
+++ node_express/routes/dish_router.js
@@ -11,35 +11,33 @@ dishRouter.route('/')
     next();
 })
 .get((req, res, next) => {
-    res.end('Will send all the dishes')
+    res.end('Will send all the dishes to you!')
 })
 .post((req, res, next) => {
     res.end(`Will add the dish: ${req.body.name} with details: ${req.body.description}`);
 })
 .put((req, res, next) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on dishes');
+    res.end('PUT operation not supported on /dishes');
 })
 .delete((req, res, next) => {
-    res.end('Deleting all the dishes');
+    res.end('Deleting all dishes');
+});
+
+dishRouter.route('/:dishId')
+.get((req, res, next) => {
+    res.end(`Will send the details of the dish: ${req.params.dishId} to you!`)
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on dishes/${req.params.dishId}`);
+})
+.put((req, res, next) => {
+    res.write(`Updating the dish: ${req.params.dishId} \n`)
+    res.end(`Will update dish: ${req.body.name} with details: ${req.body.description}`);
+})
+.delete((req, res, next) => {
+    res.end(`Deleting dish: ${req.params.dishId}`);
 });
 
 module.exports = dishRouter;
-
-
-// Can I add these back in as they are:
-//
-// app.get('/dishes/:dishId', (req, res, next) => {
-//     res.end(`Will send the dish: ${req.params.dishId}`)
-// });
-// app.post('/dishes/:dishId', (req, res, next) => {
-//     res.statusCode = 403;
-//     res.end(`POST operation not supported on dishes/${req.params.dishId}`);
-// });
-// app.put('/dishes/:dishId', (req, res, next) => {
-//     res.write(`Updating the dish: ${req.params.dishId} \n`)
-//     res.end(`Will update dish: ${req.params.dishId} with details: ${req.body.description}`);
-// });
-// app.delete('/dishes/:dishId', (req, res, next) => {
-//     res.end(`Deleting dish: ${req.params.dishId}`);
-// });
